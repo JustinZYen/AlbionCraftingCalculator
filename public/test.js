1b2bd53c95df425c4d2f9c2bae448a60afb35095
Justin Yen <justzyen@gmail.com>
2024-02-16 16:26:29 -0700

    Added in a drawing canvas

--- public/test.js
+++ public/test.js
@@ -1,1 +1,18 @@
-alert("hello");
+const movement = (event) => {
+    //alert("moved");
+    if (mouseDown) {
+        ctx.lineTo(event.offsetX,event.offsetY);
+        ctx.stroke();
+    } else {
+        ctx.beginPath();
+    }
+}
+//alert("hello");
+const myCanvas = document.getElementById("testCanvas");
+const ctx = myCanvas.getContext("2d");
+ctx.beginPath();
+ctx.moveTo(0,0);
+let mouseDown = false;
+myCanvas.addEventListener('mousemove',movement,false);
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;

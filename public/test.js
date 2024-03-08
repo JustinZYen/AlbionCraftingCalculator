44206ce1eed8b44424466974888e068fa6a75cce
RenegadePenguino <renegadepenguinelite@gmail.com>
2024-03-07 17:59:25 -0800

    remove extraneous import statement

--- public/test.js
+++ public/test.js
@@ -1,4 +1,3 @@
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"; 
 import {db} from "./firebaseScripts.js";
 
 const querySnapshot = await getDocs(collection(db, "textfiles"));

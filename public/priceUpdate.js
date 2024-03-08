ff6e945e3c5de4be4aa2f358cf4ca300239fe88a
RenegadePenguino <renegadepenguinelite@gmail.com>
2024-03-08 14:04:49 -0800

    get time

--- public/priceUpdate.js
+++ public/priceUpdate.js
@@ -2,6 +2,6 @@ import { collection, getDocs, doc, updateDoc} from "https://www.gstatic.com/fire
 import { db } from "./firebaseScripts.js"
 
 const targetDocument = doc(db,"textfiles","test");
-const date = new Date(Date.now()).toDateString();
-console.log(`Date: ${date}`);
+const date = new Date(Date.now()).getTime();
+console.log(`Time: ${date}`);
 await updateDoc(targetDocument, {time:date});

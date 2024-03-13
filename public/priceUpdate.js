112c1069b2b6ab6fe7ef589895e70c53e9423d5d
RenegadePenguino <renegadepenguinelite@gmail.com>
2024-03-12 17:41:09 -0700

    remove unneeded page

--- public/priceUpdate.js
+++
@@ -1,8 +0,0 @@
-import { collection, getDocs, doc, updateDoc} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"; 
-import { db } from "./firebaseScripts.js"
-
-const targetDocument = doc(db,"textfiles","test");
-const date = new Date(Date.now())
-
-console.log(`Time: ${date.toUTCString()}`);
-await updateDoc(targetDocument, {time:date.toUTCString()});

9a7b86e8b2b8a079a535499a974ae9d524c927b9
RenegadePenguino <renegadepenguinelite@gmail.com>
2024-03-26 11:07:01 -0700

    accessing database

--- public/test.js
+++ public/test.js
@@ -1,39 +1,21 @@
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"; 
+import { collection, getDocs, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"; 
 import {db} from "./firebaseScripts.js";
 
 
-const querySnapshot = await getDocs(collection(db, "textfiles"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-});
-
-
-const movement = (event) => {
-    //alert("moved");
-    if (mouseDown) {
-        ctx.lineTo(event.offsetX,event.offsetY);
-        ctx.stroke();
-    } else {
-        ctx.beginPath();
-    }
-}
-async function logJSON() {
-    const response = await fetch("https://west.albion-online-data.com/api/v2/stats/prices/T4_BAG,T5_BAG?locations=Caerleon,Bridgewatch&qualities=2");
-    const theJSON = await response.json();
-    console.log(theJSON);
+async function getIDFromName() {
+    const document = doc(db,"Test","One");
+    const result = await getDoc(document);
+    alert(JSON.stringify(result));
 }
 
-//logJSON();
-const myCanvas = document.getElementById("testCanvas");
-const ctx = myCanvas.getContext("2d");
-ctx.beginPath();
-ctx.moveTo(0,0);
-let mouseDown = false;
-myCanvas.addEventListener('mousemove',movement,false);
-document.body.onmousedown = () => mouseDown = true;
-document.body.onmouseup = () => mouseDown = false;
 
+function calculateProfit(itemID) {
+    
+}
+/*
 $("#my-button").on("click", () => {
     alert($("#item-name").val());
 });
+*/
 
+$("#my-button").on("click", getIDFromName);

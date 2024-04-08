6aebbe5e857033c1d1dbd8aa476e62c0a1c8b8a4
RenegadePenguino <renegadepenguinelite@gmail.com>
2024-04-07 21:32:50 -0700

    fixed error with item class where enchantment was set to NaN for enchantment level 0 items

--- public/test.js
+++ public/test.js
@@ -1,41 +1,84 @@
 import { collection, getDocs, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"; 
 import {db} from "./firebaseScripts.js";
+const MIN_TIER = 4;
+const MAX_TIER = 8;
+const nameToIDDoc = await getDoc(doc(db,"General/Item Data/Name Conversions/Name To ID"));
+const nameToID = nameToIDDoc.data();
+const idToNameDoc = await getDoc(doc(db,"General/Item Data/Name Conversions/ID To Name"));
+const idToName = idToNameDoc.data();
+const recipeDoc = await getDoc(doc(db,"General/Item Data/Recipes/Recipes"));
+const recipes = recipeDoc.data();
+const itemsList = await fetch("https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/items.json");
+const itemsJSON = await itemsList.json();
+const names = Object.keys(nameToIDDoc.data());
+let priceQueue = []; // Array of item ids that still need their prices calculated
+const items = new Map(); // HashMap of all items so far (for saving prices);
 
 class Item {
     // Price will be instantiated as a group to reduce api calls
     price = NaN;
     tier = NaN;
     enchantment = 0;
+    id;
+    baseId;
+    // Recipes will be array of recipes, which will each be arrays of "tuples"
+    recipes = null;
     constructor(id) {
         this.id = id;
+        this.#setbaseId();
         this.#setTier();
         this.#setEnchantment();
+        this.#setRecipes();
     }
 
+    #setbaseId() {
+        const convertedID = nameToID[idToName[this.id]];
+        if (typeof convertedID == String) {
+            this.baseId = convertedID;
+        } else {
+            this.baseId = convertedID[0];
+        }
+    }
     #setTier() {
         const secondValue = parseInt(this.id.charAt(1));
         if (this.id.charAt(0) === "T" && secondValue != NaN) {
             this.tier = secondValue;
         } else {
-            console.log(`Id ${id} has no tier found`);
+            console.log(`Id ${this.id} has no tier found`);
         }
     }
 
     #setEnchantment() {
         const lastVal = parseInt(this.id.charAt(this.id.length-1));
-        if (lastVal != NaN) {
+        console.log(`lastVal: ${lastVal}`);
+        if (!isNaN(lastVal)) {
             this.enchantment = lastVal;
         }
     }
 
+    #setRecipes() {
+        console.log(`id: ${this.id}`);
+        const path = recipes[this.id];
+        if (path == null) {
+            console.log(`No path found for id ${this.id}`);
+            return;
+        }
+        let itemInfo = itemsJSON;
+        path.forEach(element=>{
+            itemInfo = itemInfo[element];
+        })
+        console.log(itemInfo);
+    }
+
     toString() {
-        return `id: ${id}, tier: ${tier}, enchantment: ${enchantment}, price: ${price}`;
+        return `id: ${id}, base id: ${baseId}, tier: ${tier}, enchantment: ${enchantment}, price: ${price}`;
     }
 }
 
 function getIDFromName() {
     const input = $("#item-name").val();
     console.log(input);
+    // If input value is contained in nameToID
     if (nameToID.hasOwnProperty(input)) {
         let ids = structuredClone(nameToID[input]);
         console.log(ids);
@@ -52,7 +95,12 @@ function getIDFromName() {
                 }
             }
         });
-        console.log(ids);
+        //console.log(ids);
+        let items = new Set();
+        ids.forEach(element=>{
+            items.add(new Item(element));
+        });
+        console.log(items);
        //getAveragePrices();
     }
 
@@ -68,17 +116,7 @@ function calculateProfit(itemID,tax) {
     return (1-tax)*sellPrice-craftPrice;
 }
 
-// Get document of names to ids
-// Get document of recipe paths
-const MIN_TIER = 4;
-const MAX_TIER = 8;
-const nameToIDDoc = await getDoc(doc(db,"General/Item Data/Name Conversions/Name To ID"));
-const nameToID = nameToIDDoc.data();
-const recipeDoc = await getDoc(doc(db,"General/Item Data/Recipes/Recipes"));
-const recipes = recipeDoc.data();
-const names = Object.keys(nameToIDDoc.data());
-let priceQueue = []; // Array of item ids that still need their prices calculated
-const items = new Map(); // HashMap of all items so far (for saving prices);
+
 $("#my-button").on("click", getIDFromName);
 $( "#item-name" ).autocomplete({
     source: names

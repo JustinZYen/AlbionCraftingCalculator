54e085bc31a31e59ac5323842ad63644f25e25f3
RenegadePenguino <renegadepenguinelite@gmail.com>
2024-05-20 13:37:41 -0700

    Reapply "Reverting the previous revert called "Revert "revert""""
    This reverts commit 8ad81831e7efea00e03774611bc559797c782e89.

--- public/test.js
+++ public/test.js
@@ -1,4 +1,5 @@
 "use strict";
+//test
 import { collection, getDocs, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"; 
 import {db} from "./firebaseScripts.js";
 class DateEnum {
@@ -318,12 +319,17 @@ async function getProfits() {
                     }
                 }
                 uncheckedItems.set(currentItem.priceId,currentItem);
+                checkedItems.set(currentItem.priceId,currentItem);
             }
         }
         //console.log(uncheckedItems)
         console.log("setting prices");
         await setPrices(uncheckedItems);
+<<<<<<< HEAD
+        /*
+=======
         
+>>>>>>> parent of 7020eb3 (Prints out recipes (formatting is poor))
         checkedItems.forEach((value,key)=> {
             console.log(`key: ${key}, value: ${value}`);
         });
@@ -420,6 +426,10 @@ async function setPrices(uncheckedItems) {
             currentItemString = currentItem.id;
         }
     });
+    if (currentItemString === "") {
+        console.log("No more new prices to collect.");
+        return;
+    }
     await getPrices(PRICE_URL_START+currentItemString+PRICE_URL_END_OLD,DateEnum.OLD);
     await getPrices(PRICE_URL_START+currentItemString+PRICE_URL_END_NEW,DateEnum.NEW);
     uncheckedItems.clear();
@@ -441,6 +451,7 @@ async function getPrices(priceURL,timeSpan) {
             const currentPriceId = currentItem.item_id;
             let targetItem; 
             if (!checkedItems.has(currentPriceId)) {
+                console.log("priceId "+currentPriceId+" was not added to checkedItems");
                 targetItem = new Item(currentPriceId);
                 checkedItems.set(currentPriceId,targetItem);
             } else {
@@ -481,8 +492,9 @@ async function getPrices(priceURL,timeSpan) {
 }
 
 function displayRecipes(ids) {
-    for (const currentId of ids) {
-        
+    for (const currentPriceId of ids) {
+        console.log(currentPriceId);
+        console.log(checkedItems.get(currentPriceId));
     }
 }
 

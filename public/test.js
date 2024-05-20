4fc53a6d4e5089166cb94b6e6a8346449c30613e
RenegadePenguino <renegadepenguinelite@gmail.com>
2024-05-20 14:39:47 -0700

    Fixed issues with recipes not showing up

--- public/test.js
+++ public/test.js
@@ -150,7 +150,7 @@ class Item {
             //let upgradeRequirements = itemInfo.upgraderequirements;
             let previousId;
             if (this.enchantment === 1) {
-                previousId = this.priceId;
+                previousId = this.priceId.slice(0,-1);
             } else {
                 previousId = this.priceId.slice(0,-1)+(this.enchantment-1);
             }
@@ -324,17 +324,13 @@ async function getProfits() {
         //console.log(uncheckedItems)
         console.log("setting prices");
         await setPrices(uncheckedItems);
-<<<<<<< HEAD
-        /*
-=======
         
->>>>>>> parent of 7020eb3 (Prints out recipes (formatting is poor))
+        /*
         checkedItems.forEach((value,key)=> {
             console.log(`key: ${key}, value: ${value}`);
         });
-        if ($("#title").val() != null) {
-            displayRecipes(ids)
-        }
+        */
+        displayRecipes(ids)
        //getAveragePrices();
     } else {
         console.log(`input string ${input} not found`);
@@ -491,9 +487,22 @@ async function getPrices(priceURL,timeSpan) {
 }
 
 function displayRecipes(ids) {
+    function recipeHelper(id) {
+        let returnString = `name: ${idToName[id]} `;
+        if (checkedItems.get(id).recipes.length == 0) {
+            return returnString;
+        }
+        returnString += "recipe(s): ("
+        for (const recipe of checkedItems.get(id).recipes) {
+            for (const resource of recipe.resources) {
+                returnString += recipeHelper(resource[0]) + "amount: " + resource[1]+" ";
+            }
+            returnString += " ; ";
+        }
+        return returnString + ")";
+    }
     for (const currentPriceId of ids) {
-        console.log(currentPriceId);
-        console.log(checkedItems.get(currentPriceId));
+        console.log(recipeHelper(currentPriceId));
     }
 }
 

5c6440826d9a91eee0aec100fb6bd9fcf102bf66
RenegadePenguino <justzyen@gmail.com>
2024-10-11 21:03:51 -0700

    Setup typescript for project

--- typescript/firebaseScripts.ts
+++ typescript/firebaseScripts.ts
@@ -4,19 +4,21 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDkCQTICKkqd7SQqj9ZVSen4qwlkUtV9bY",
-    authDomain: "albion-project-9585a.firebaseapp.com",
-    projectId: "albion-project-9585a",
-    storageBucket: "albion-project-9585a.appspot.com",
-    messagingSenderId: "897223201752",
-    appId: "1:897223201752:web:b82536955d7eb8344dd8ad",
-    measurementId: "G-243WD30YNN",
+  apiKey: "AIzaSyDkCQTICKkqd7SQqj9ZVSen4qwlkUtV9bY",
+  authDomain: "albion-project-9585a.firebaseapp.com",
+  projectId: "albion-project-9585a",
+  storageBucket: "albion-project-9585a.appspot.com",
+  messagingSenderId: "897223201752",
+  appId: "1:897223201752:web:b82536955d7eb8344dd8ad",
+  measurementId: "G-243WD30YNN",
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
-export { app, analytics, db };
+export {app, analytics,db};

7b18d04e1810b3a41625eb5bd0d3bdad9af4219e
RenegadePenguino <justzyen@gmail.com>
2024-10-22 10:51:42 -0700

    Separate item price data into separate class (requires additional changes for better proejct design

---
+++ typescript/item-data.ts
@@ -0,0 +1,206 @@
+import { nameToID } from "./external-data.js";
+import { db } from "./firebaseScripts.js";
+import { DateEnum,Item } from "./item.js";
+import { collection, getDocs, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"; 
+
+class ItemData {
+    // HashMap of all Items so far (for saving prices)
+    // Uses priceIds as keys
+    checkedItems = new Map();
+    ids:string[] = [];
+    async getProfits() {
+        document.getElementById("recipes-area")!.innerHTML = "";
+        const input: string = ($("#item-name").val()) as string;
+        console.log("Input value: " + input);
+        // If input value is contained in nameToID
+        if (nameToID.hasOwnProperty(input)) {
+            this.ids = this.getItemIds(input);
+            this.ids.sort();
+            // Set containing all strings for which prices need to be determined
+            let uncheckedItems = new Map<string, Item>();
+            // Stack containing items that still need to be determined whether a price check is needed
+            let itemStack: Item[] = [];
+            // Set up stack with all items in ids array
+            for (const priceId of this.ids) {
+                itemStack.push(new Item(priceId));
+            }
+            while (itemStack.length > 0) {
+                const currentItem = itemStack.pop()!;
+                // console.log(`current item: ${currentItem}`);
+                // If current item is not in checked or unchecked items
+                if (!uncheckedItems.has(currentItem.priceId) && !this.checkedItems.has(currentItem.priceId)) {
+                    //console.log(`currentItem: ${typeof currentItem}`);
+                    for (const recipe of currentItem.recipes) {
+                        for (const resource of recipe.resources) {
+                            itemStack.push(new Item(resource.priceId));
+                        }
+                    }
+                    uncheckedItems.set(currentItem.priceId, currentItem);
+                    this.checkedItems.set(currentItem.priceId, currentItem);
+                }
+            }
+            //console.log(uncheckedItems)
+            console.log("setting prices");
+            await this.setPrices(uncheckedItems);
+            //getAveragePrices();
+        } else {
+            console.log(`input string ${input} not found`);
+        }
+    }
+
+    /**
+ * 
+ * @param {string} priceURL URL needed for api, not including timescale
+ * @param {DateEnum} timeSpan OLD or NEW, representing previous patch or current patch, respectively
+ */
+    async getPrices(priceURL: string, timeSpan: DateEnum) {
+        function fixLocation(initialLocation: string) {
+            if (initialLocation == "5003") {
+                return "Brecilien";
+            } else {
+                return initialLocation;
+            }
+        }
+        console.log("Price url: " + priceURL);
+        const timescales = [1, 6, 24]
+        for (const timescale of timescales) {
+            let priceContents = await fetch(priceURL + timescale);
+            let priceContentsJSON = await priceContents.json();
+            // Check timescale and update prices if timescale is higher
+            for (const currentItem of priceContentsJSON) {
+                const currentPriceId = currentItem.item_id;
+                let targetItem;
+                if (!this.checkedItems.has(currentPriceId)) {
+                    console.log("priceId " + currentPriceId + " was not added to checkedItems");
+                    targetItem = new Item(currentPriceId);
+                    this.checkedItems.set(currentPriceId, targetItem);
+                } else {
+                    targetItem = this.checkedItems.get(currentPriceId);
+                }
+                // Get prices; set to appropriate location
+                const location = fixLocation(currentItem["location"]);
+                const quality = currentItem["quality"];
+                //console.log("target item: "+targetItem);
+                const priceInfo = targetItem.priceInfos.get(timeSpan);
+                if (quality <= 2) {
+                    if (!priceInfo.priceQualities.has(location) || quality >= priceInfo.priceQualities.get(location)) {
+                        // add price if timescale is better
+                        const data = currentItem["data"];
+                        // Find timescale difference
+                        const startDate = data[0]["timestamp"];
+                        const endDate = data[data.length - 1]["timestamp"];
+                        const timescale = (Date.parse(endDate) - Date.parse(startDate));
+                        // console.log(`start date: ${startDate}, end date: ${endDate}`);
+                        if (!priceInfo.priceTimescale.has(location) || timescale > priceInfo.priceTimescale.get(location)) {
+                            let total = 0;
+                            for (const timestampData of data) {
+                                total += timestampData["avg_price"];
+                            }
+                            const average = total / data.length;
+                            //console.log("average: "+average);
+                            priceInfo.price.set(location, average);
+                            //console.log("Updating with new prices")
+                        }
+
+                        // Find timescale
+                    }
+                }
+
+            }
+            console.log("Done with timescale " + timescale);
+        }
+    }
+/**
+ * 
+ * @param {String} itemId 
+ * @returns An array containing item ids of all weapons that share the name in the tree
+ */
+ getItemIds(itemId:string) {
+    const MIN_TIER = 4;
+    const MAX_TIER = 8;
+    let ids:string[] = structuredClone(nameToID[itemId]);
+    ids.forEach((element,_,array) => {
+        const secondValue = parseInt(element.charAt(1));
+        if (element.charAt(0) === "T" && !Number.isNaN(secondValue)) {
+            // Current item has different tiers since it is T-some number
+            const stringRemainder = element.slice(2);
+            for (let i = MIN_TIER; i <= MAX_TIER; i++) {
+                if (i != secondValue) {
+                    array.push("T"+i+stringRemainder);
+                }
+            }
+        }
+    });
+    return ids;
+}
+
+/**
+ * 
+ * @param {Set} uncheckedItems A set containing Items representing all items for which prices have not yet been calculated
+ */
+async setPrices(uncheckedItems: Map<string,Item>) {
+    const PRICE_URL_START = "https://west.albion-online-data.com/api/v2/stats/history/";
+    const PRICE_URL_END_OLD = await this.previousDateString()+"&locations=0007,1002,2004,3005,3008,4002,5003&time-scale=";
+    const PRICE_URL_END_NEW = await this.currentDateString()+"&locations=0007,1002,2004,3005,3008,4002,5003&time-scale=";
+    const startStringLength = PRICE_URL_START.length;
+    const endStringLength = Math.max(PRICE_URL_END_OLD.length,PRICE_URL_END_NEW.length);
+    const MAX_URL_LENGTH = 4096;
+    // Note: Missing time scale so that I can test out all 3 possible timescales
+    let currentItemString = "";
+    uncheckedItems.forEach(async currentItem => {
+        // Check if more prices can fit into current URL
+        let currentPriceId = currentItem.priceId;
+        if (currentItemString.length + currentPriceId.length < MAX_URL_LENGTH) {
+            if (currentItemString.length == 0) {
+                currentItemString = currentPriceId;
+            } else {
+                currentItemString += (","+currentPriceId);
+            }
+        } else {
+            await this.getPrices(PRICE_URL_START+currentItemString+PRICE_URL_END_OLD,DateEnum.OLD);
+            await this.getPrices(PRICE_URL_START+currentItemString+PRICE_URL_END_NEW,DateEnum.NEW);
+            currentItemString = currentItem.id;
+        }
+    });
+    if (currentItemString === "") {
+        console.log("No more new prices to collect.");
+        return;
+    }
+    await this.getPrices(PRICE_URL_START+currentItemString+PRICE_URL_END_OLD,DateEnum.OLD);
+    await this.getPrices(PRICE_URL_START+currentItemString+PRICE_URL_END_NEW,DateEnum.NEW);
+    uncheckedItems.clear();
+}
+async  previousDateString() {
+    const patchDateDoc = await getDoc(doc(db,"General/Patch Data"));
+    const patchDates = await patchDateDoc.data();
+    const previousPatchDateDate = new Date(patchDates["Previous Date"]);
+    const previousPatchDateString = previousPatchDateDate.getUTCFullYear()+"-"+
+        (previousPatchDateDate.getUTCMonth()+1)+"-"+
+        (previousPatchDateDate.getUTCDate());
+    const patchDateDate = new Date(patchDates.Date);
+    const patchDateString = patchDateDate.getUTCFullYear()+"-"+
+        (patchDateDate.getUTCMonth()+1)+"-"+
+        (patchDateDate.getUTCDate());
+    return this.dateString(previousPatchDateString,patchDateString);
+}
+
+async  currentDateString() {
+    const patchDateDoc = await getDoc(doc(db,"General/Patch Data"));
+    const patchDates = await patchDateDoc.data();
+    const previousPatchDateDate = new Date(patchDates.Date);
+    const previousPatchDateString = previousPatchDateDate.getUTCFullYear()+"-"+
+        (previousPatchDateDate.getUTCMonth()+1)+"-"+
+        (previousPatchDateDate.getUTCDate());
+    const currentDateDate = new Date();
+    const currentDateString = currentDateDate.getUTCFullYear()+"-"+
+        (currentDateDate.getUTCMonth()+1)+"-"+
+        (currentDateDate.getUTCDate());
+    return this.dateString(previousPatchDateString,currentDateString);
+
+}
+ dateString(startDate:string,endDate:string) {
+    return "?date="+startDate+"&end_date="+endDate;
+}
+}
+
+export { ItemData };
--- typescript/test.ts
+++ typescript/test.ts
@@ -4,14 +4,13 @@ import {db} from "./firebaseScripts.js";
 import { DateEnum,Item } from "./item.js";
 import { idToName, names, nameToID } from "./external-data.js";
 import { RecipeBox, ItemBox } from "./display-boxes.js";
+import { ItemData } from "./item-data.js";
 class CraftTypeEnum {
     static REFINING = Symbol("Refining");
     static CRAFTING = Symbol("Crafting")
 }
 
-// HashMap of all Items so far (for saving prices)
-// Uses priceIds as keys
-const checkedItems = new Map(); 
+
 // HashMap of all the ItemBoxes that correspond to a certain item
 // Uses priceIds as keys and an ItemBox array as value
 const itemBoxes = new Map();
@@ -52,204 +51,14 @@ const cityBonuses = new Map([
 
 
 
-async function getProfits() {
-    document.getElementById("recipes-area")!.innerHTML = "";
-    const input:string = ($("#item-name").val()) as string;
-    console.log("Input value: "+input);
-    // If input value is contained in nameToID
-    if (nameToID.hasOwnProperty(input)) {
-        let ids = getItemIds(input);
-        ids.sort();
-        // Set containing all strings for which prices need to be determined
-        let uncheckedItems = new Map<string,Item>();
-        // Stack containing items that still need to be determined whether a price check is needed
-        let itemStack:Item[] = [];
-        // Set up stack with all items in ids array
-        for (const priceId of ids) {
-            itemStack.push(new Item(priceId));
-        }
-        while (itemStack.length > 0) {
-            const currentItem = itemStack.pop()!;
-            // console.log(`current item: ${currentItem}`);
-            // If current item is not in checked or unchecked items
-            if (!uncheckedItems.has(currentItem.priceId) && !checkedItems.has(currentItem.priceId)) {
-                //console.log(`currentItem: ${typeof currentItem}`);
-                for (const recipe of currentItem.recipes) {
-                    for (const resource of recipe.resources) {
-                        itemStack.push(new Item(resource.priceId));
-                    }
-                }
-                uncheckedItems.set(currentItem.priceId,currentItem);
-                checkedItems.set(currentItem.priceId,currentItem);
-            }
-        }
-        //console.log(uncheckedItems)
-        console.log("setting prices");
-        await setPrices(uncheckedItems);
-        displayRecipes(ids)
-       //getAveragePrices();
-    } else {
-        console.log(`input string ${input} not found`);
-    }
-}
 
-async function previousDateString() {
-    const patchDateDoc = await getDoc(doc(db,"General/Patch Data"));
-    const patchDates = await patchDateDoc.data();
-    const previousPatchDateDate = new Date(patchDates["Previous Date"]);
-    const previousPatchDateString = previousPatchDateDate.getUTCFullYear()+"-"+
-        (previousPatchDateDate.getUTCMonth()+1)+"-"+
-        (previousPatchDateDate.getUTCDate());
-    const patchDateDate = new Date(patchDates.Date);
-    const patchDateString = patchDateDate.getUTCFullYear()+"-"+
-        (patchDateDate.getUTCMonth()+1)+"-"+
-        (patchDateDate.getUTCDate());
-    return dateString(previousPatchDateString,patchDateString);
-}
 
-async function currentDateString() {
-    const patchDateDoc = await getDoc(doc(db,"General/Patch Data"));
-    const patchDates = await patchDateDoc.data();
-    const previousPatchDateDate = new Date(patchDates.Date);
-    const previousPatchDateString = previousPatchDateDate.getUTCFullYear()+"-"+
-        (previousPatchDateDate.getUTCMonth()+1)+"-"+
-        (previousPatchDateDate.getUTCDate());
-    const currentDateDate = new Date();
-    const currentDateString = currentDateDate.getUTCFullYear()+"-"+
-        (currentDateDate.getUTCMonth()+1)+"-"+
-        (currentDateDate.getUTCDate());
-    return dateString(previousPatchDateString,currentDateString);
 
-}
-function dateString(startDate:string,endDate:string) {
-    return "?date="+startDate+"&end_date="+endDate;
-}
 
-/**
- * 
- * @param {String} itemId 
- * @returns An array containing item ids of all weapons that share the name in the tree
- */
-function getItemIds(itemId:string) {
-    const MIN_TIER = 4;
-    const MAX_TIER = 8;
-    let ids:string[] = structuredClone(nameToID[itemId]);
-    ids.forEach((element,_,array) => {
-        const secondValue = parseInt(element.charAt(1));
-        if (element.charAt(0) === "T" && !Number.isNaN(secondValue)) {
-            // Current item has different tiers since it is T-some number
-            const stringRemainder = element.slice(2);
-            for (let i = MIN_TIER; i <= MAX_TIER; i++) {
-                if (i != secondValue) {
-                    array.push("T"+i+stringRemainder);
-                }
-            }
-        }
-    });
-    return ids;
-}
 
-/**
- * 
- * @param {Set} uncheckedItems A set containing Items representing all items for which prices have not yet been calculated
- */
-async function setPrices(uncheckedItems: Map<string,Item>) {
-    const PRICE_URL_START = "https://west.albion-online-data.com/api/v2/stats/history/";
-    const PRICE_URL_END_OLD = await previousDateString()+"&locations=0007,1002,2004,3005,3008,4002,5003&time-scale=";
-    const PRICE_URL_END_NEW = await currentDateString()+"&locations=0007,1002,2004,3005,3008,4002,5003&time-scale=";
-    const startStringLength = PRICE_URL_START.length;
-    const endStringLength = Math.max(PRICE_URL_END_OLD.length,PRICE_URL_END_NEW.length);
-    const MAX_URL_LENGTH = 4096;
-    // Note: Missing time scale so that I can test out all 3 possible timescales
-    let currentItemString = "";
-    uncheckedItems.forEach(async currentItem => {
-        // Check if more prices can fit into current URL
-        let currentPriceId = currentItem.priceId;
-        if (currentItemString.length + currentPriceId.length < MAX_URL_LENGTH) {
-            if (currentItemString.length == 0) {
-                currentItemString = currentPriceId;
-            } else {
-                currentItemString += (","+currentPriceId);
-            }
-        } else {
-            await getPrices(PRICE_URL_START+currentItemString+PRICE_URL_END_OLD,DateEnum.OLD);
-            await getPrices(PRICE_URL_START+currentItemString+PRICE_URL_END_NEW,DateEnum.NEW);
-            currentItemString = currentItem.id;
-        }
-    });
-    if (currentItemString === "") {
-        console.log("No more new prices to collect.");
-        return;
-    }
-    await getPrices(PRICE_URL_START+currentItemString+PRICE_URL_END_OLD,DateEnum.OLD);
-    await getPrices(PRICE_URL_START+currentItemString+PRICE_URL_END_NEW,DateEnum.NEW);
-    uncheckedItems.clear();
-}
 
-/**
- * 
- * @param {string} priceURL URL needed for api, not including timescale
- * @param {DateEnum} timeSpan OLD or NEW, representing previous patch or current patch, respectively
- */
-async function getPrices(priceURL:string,timeSpan: DateEnum) {
-    function fixLocation(initialLocation:string) {
-        if (initialLocation == "5003") {
-            return "Brecilien";
-        } else {
-            return initialLocation;
-        }
-    }
-    console.log("Price url: "+priceURL);
-    const timescales = [1,6,24]
-    for (const timescale of timescales) {
-        let priceContents = await fetch(priceURL+timescale);
-        let priceContentsJSON = await priceContents.json();
-        // Check timescale and update prices if timescale is higher
-        for (const currentItem of priceContentsJSON) {
-            const currentPriceId = currentItem.item_id;
-            let targetItem; 
-            if (!checkedItems.has(currentPriceId)) {
-                console.log("priceId "+currentPriceId+" was not added to checkedItems");
-                targetItem = new Item(currentPriceId);
-                checkedItems.set(currentPriceId,targetItem);
-            } else {
-                targetItem = checkedItems.get(currentPriceId);
-            }
-            // Get prices; set to appropriate location
-            const location = fixLocation(currentItem["location"]);
-            const quality = currentItem["quality"];
-            //console.log("target item: "+targetItem);
-            const priceInfo = targetItem.priceInfos.get(timeSpan);
-            if (quality <= 2) {
-                if (!priceInfo.priceQualities.has(location) || quality >= priceInfo.priceQualities.get(location)) {
-                    // add price if timescale is better
-                    const data = currentItem["data"];
-                    // Find timescale difference
-                    const startDate = data[0]["timestamp"];
-                    const endDate = data[data.length-1]["timestamp"];
-                    const timescale = (Date.parse(endDate)-Date.parse(startDate));
-                    // console.log(`start date: ${startDate}, end date: ${endDate}`);
-                    if (!priceInfo.priceTimescale.has(location) || timescale > priceInfo.priceTimescale.get(location)) {
-                        let total = 0;
-                        for (const timestampData of data) {
-                            total += timestampData["avg_price"];
-                        }
-                        const average = total/data.length;
-                        //console.log("average: "+average);
-                        priceInfo.price.set(location,average);
-                        //console.log("Updating with new prices")
-                    }
-                    
-                    // Find timescale
-                }
-            }
-            
-        }
-        console.log("Done with timescale "+ timescale);
-    }
-}
 
-function displayRecipes(ids:string[]) {
+function displayRecipes(checkedItems: Map<any, any>, ids: string[]) {
     for (const currentPriceId of ids) {
         const currentItem = checkedItems.get(currentPriceId);
 
@@ -462,19 +271,24 @@ function calculateProfit(itemID:string,tax:number) {
 }
 */
 
-document.getElementById("load-price-button")?.addEventListener("click",getProfits);
+const itemData = new ItemData();
+
+document.getElementById("load-price-button")?.addEventListener("click",async ()=>{
+    await itemData.getProfits();
+    displayRecipes(itemData.checkedItems,itemData.ids);
+});
 
 (<any>$( "#item-name" )).autocomplete({
     source: names
 });
 
 $("#city-selector").on("change",()=>{
-    getProfits();
+    itemData.getProfits();
     console.log($("#city-selector").val());
 });
 
 $("#date-selector").on("change",()=>{
-    getProfits();
+    itemData.getProfits();
     console.log($("#date-selector").is(":checked"));
 });
 

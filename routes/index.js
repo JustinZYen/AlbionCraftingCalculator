326585e6869226123cb8db00114078f0bd4795bd
Justin Yen <justzyen@gmail.com>
2024-12-19 20:30:34 -0800

    move rendering of crafting bonuses into ejs

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { stationNames } from "../public/javascripts/globals/constants.js";
+import { stationNames, cityBonuses } from "../public/javascripts/globals/constants.js";
 var router = Router();
 /* GET home page. */
 router.get('/', function (_req, res, _next) {
-    res.render('index', { stationNames: stationNames });
+    res.render('index', { stationNames: stationNames, cityBonuses: cityBonuses });
 });
 export default router;
--- typescript/routes/index.ts
+++ typescript/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import {stationNames} from "../public/javascripts/globals/constants.js"
+import { stationNames, cityBonuses } from "../public/javascripts/globals/constants.js"
 
 var router = Router();
 
 /* GET home page. */
-router.get('/', function(_req, res, _next) {
-  res.render('index', { stationNames: stationNames });
+router.get('/', function (_req, res, _next) {
+  res.render('index', { stationNames: stationNames, cityBonuses: cityBonuses });
 });
 
 export default router;

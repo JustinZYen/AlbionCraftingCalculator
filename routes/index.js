8f1df949f3244a1e87c0374f649913d16826e56f
Justin Yen <justzyen@gmail.com>
2024-12-19 20:04:54 -0800

    move rendering of crafting fees into ejs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import { stationNames } from "../public/javascripts/globals/constants.js";
 var router = Router();
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', function (_req, res, _next) {
+    res.render('index', { stationNames: stationNames });
 });
 export default router;

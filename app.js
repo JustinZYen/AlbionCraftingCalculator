3f0fb2672816c1ffa6b53d3496299d38224220cc
JustinZYen <justzyen@gmail.com>
2025-01-06 20:27:18 -0800

    update with helmetjs

--- app.js
+++ app.js
@@ -4,12 +4,29 @@ import { fileURLToPath } from 'url';
 import { join, dirname } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import helmet from "helmet";
 import indexRouter from './routes/index.js';
 var app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 // view engine setup
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            frameAncestors: ["https://justinyen.azurewebsites.net/"], // Allow my portfolio website to embed this site as iframe
+            scriptSrc: [
+                "'self'",
+                "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js",
+                "https://code.jquery.com/ui/1.13.2/jquery-ui.js",
+                "https://cdn.jsdelivr.net/npm/d3@7",
+                "https://www.gstatic.com/firebasejs/10.8.0/",
+                'https://www.googletagmanager.com/gtag/'
+            ],
+            defaultSrc: helmet.contentSecurityPolicy.dangerouslyDisableDefaultSrc // Didn't want to deal with inputting all the firebase urls as I am planning on removing the firebase component
+        }
+    }
+}));
 app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));

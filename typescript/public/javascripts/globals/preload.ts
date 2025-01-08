988f0f7675cf3a3ab8c0363a94c4e0d6d04e2185
JustinZYen <justzyen@gmail.com>
2025-01-08 15:35:18 -0800

    removed variable that was not being used

--- typescript/public/javascripts/globals/preload.ts
+++ typescript/public/javascripts/globals/preload.ts
@@ -1,31 +1,5 @@
-import {stationNames} from "./constants.js"
 import {namesPromise} from "../external-data.js";
 import { ItemNameTrie } from "../classes/trie.js";
-/**
- * Maps item ids (in Albion binaries) to crafting station ids (in Albion binaries)
- * 
- * Maybe not needed, as can determine just from crafting category
- */
-const buildingsThatCanCraft = fetch("https://raw.githubusercontent.com/ao-data/ao-bin-dumps/refs/heads/master/buildings.json").then((response)=>{
-    return response.json();
-}).then((buildingsJSON)=>{
-    const craftBuildingArray = buildingsJSON.buildings.craftbuilding;
-    const stationBonuses:{[key:string]:string} = Object.create(null);
-    for (const craftBuilding of craftBuildingArray) {
-        const buildingName = craftBuilding["@uniquename"]
-        if (buildingName in stationNames) {
-            const itemsList:{
-                "@uniquename":string,
-                "displaygroup": string,
-                "@category": string
-            }[] = craftBuilding.craftingitemlist.craftitem;
-            for (const item of itemsList) {
-                stationBonuses[item["@uniquename"]] = buildingName;
-            }
-        }
-    }
-    return stationBonuses;
-});
 
 const itemNameTrie = new ItemNameTrie();
 namesPromise.then((nameData)=>{
@@ -34,4 +8,4 @@ namesPromise.then((nameData)=>{
     }
 });
 
-export {buildingsThatCanCraft,itemNameTrie};
+export {itemNameTrie};
